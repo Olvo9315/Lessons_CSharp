7c19872337ceeb0aa95acb6adeb5fb636eedc759
olvo <alexvolenko9315@gmail.com>
2023-07-21 08:31:31 +0200

    greeting user

---
+++ Example_005_ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("User name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower == "alex")
+{
+    Console.Write("I'm glad to see you, " + username + "!");
+}
+else
+{
+    Console.Write("Hello, " + username + ".");
+}
