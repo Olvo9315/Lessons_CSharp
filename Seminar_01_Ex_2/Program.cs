b4936a314dd387c9b20b04dc618a6a212c43f91c
olvo <alexvolenko9315@gmail.com>
2023-07-25 16:09:41 +0200

    Ex_2

---
+++ Seminar_01_Ex_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Семинар 1 - Задача 2
+Console.WriteLine("Give me a number A: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Give me a number B: ");
+int numberB = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine($"A = {numberA}; B = {numberB} -> max = " + Math.Max(numberA, numberB));
+
+//or with if-else: 
+// if (numberB > numberA)
+// {
+//    Console.WriteLine($"A = {numberA}; B = {numberB} -> max = {numberB}");
+// }
+// else
+// {
+//    Console.WriteLine($"A = {numberA}; B = {numberB} -> max = {numberA}");
+// }
