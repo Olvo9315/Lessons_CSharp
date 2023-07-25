8ed20d0d5754232e0662dcdc99043639a869f11a
olvo <alexvolenko9315@gmail.com>
2023-07-25 16:30:32 +0200

    Ex_8

---
+++ Seminar_01_Ex_8/Program.cs
@@ -0,0 +1,16 @@
+﻿//Семинар 1 - Задача 8
+Console.WriteLine("Give me a number: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+int i = 2;
+if (N < 1)
+{
+    Console.WriteLine("Number must be > 0");
+}
+else
+{
+    while (i <= N)
+    {
+        Console.Write($"{i} ");
+        i += 2;
+    }
+}
