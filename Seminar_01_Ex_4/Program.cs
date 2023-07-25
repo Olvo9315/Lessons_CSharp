31cf68899565144c2850ffd19bdbd4c768527f85
olvo <alexvolenko9315@gmail.com>
2023-07-25 16:16:01 +0200

    Ex_4

---
+++ Seminar_01_Ex_4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Семинар 1 - Задача 4
+Console.WriteLine("Give me a number A: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Give me a number B: ");
+int numberB = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Give me a number C: ");
+int numberC = int.Parse(Console.ReadLine() ?? "");
+int maxAB = Math.Max(numberA, numberB);
+int max = Math.Max(numberC, maxAB);
+Console.WriteLine($"max = {max}");
