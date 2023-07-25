30bf1656baed150296c19c149ef84989cee6696a
olvo <alexvolenko9315@gmail.com>
2023-07-25 16:21:53 +0200

    Ex_6

---
+++ Seminar_01_Ex_6/Program.cs
@@ -0,0 +1,10 @@
+﻿//Семинар 1 - Задача 6
+Console.WriteLine("Give me a number: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+if (number % 2 == 0)
+{
+    Console.WriteLine("Yes, it's even number");
+}
+else {
+    Console.WriteLine("No, it's not an even number");
+}
