e0fd190f00ff1115a19d4f32c232dc02b0c0feca
olvo <alexvolenko9315@gmail.com>
2023-07-25 10:48:10 +0200

    seminar_1

---
+++ S_lessons_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Give me a number A: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Give me a number B: ");
+int numberB = int.Parse(Console.ReadLine() ?? "");
+int sqrA = numberA * numberA;
+int sqrB = numberB * numberB;
+if (sqrA == numberB)
+{
+    Console.WriteLine($"{numberB} is SQR of {numberA}");
+}
+else if (sqrB == numberA)
+{
+    Console.WriteLine($"{numberA} is SQR of {numberB}");
+}
+else
+{
+    Console.WriteLine("No SQRs");
+}
