7d03bf93111f068bb6ac399a96c515cc3fc9b963
olvo <alexvolenko9315@gmail.com>
2023-07-30 15:24:05 +0200

    Seminar 2 ex 13

---
+++ Seminar_02_Ex_13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет
+
+int ThirdNumber ( int number )
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+Console.Write("Give me a number: ");
+int number = Math.Abs(int.Parse(Console.ReadLine() ?? ""));
+
+if (number < 100)
+{
+    Console.WriteLine("There is no 3ed number");
+}
+else
+{
+    Console.WriteLine($"Third number: {ThirdNumber(number)}");
+}
