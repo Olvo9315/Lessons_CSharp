5e47f3aa14f228510f15c830cd9b5cbaeb48cf67
olvo <alexvolenko9315@gmail.com>
2023-07-30 15:33:40 +0200

    Seminar 2 Ex 15

---
+++ Seminar_02_Ex_15/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int TextInput ()
+{
+    while(true)
+    {
+        System.Console.Write("Give me a number of the day (1-7): ");
+        int number = int.Parse(Console.ReadLine() ?? "");
+        if (number <= 7 && number > 0)
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine($"{number} is not a day!");
+        }
+    }
+}
+
+if (TextInput() > 5)
+{
+    Console.WriteLine("Yeaaah, it's a weekend!");
+}
+else
+{
+    Console.WriteLine("Oh, it's not a weekend :(");
+}
