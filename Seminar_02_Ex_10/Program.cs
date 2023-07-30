6b8dd23d964b0cb881d55fdb41091864918d3466
olvo <alexvolenko9315@gmail.com>
2023-07-30 14:44:22 +0200

    Seminar2, ex 10

---
+++ Seminar_02_Ex_10/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+int TextInput ()
+{
+    while(true)
+    {
+        System.Console.Write("Give me a number from 100 to 999: ");
+        int number = int.Parse(Console.ReadLine() ?? "");
+        if (number >= 100 && number <= 999)
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine($"{number} is not in [100:999]!");
+        }
+    }
+}
+
+int secondNumber = TextInput() / 10 % 10;
+Console.WriteLine($"Second number: {secondNumber}");
